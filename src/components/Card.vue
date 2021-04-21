5e4c448cd86b49793367eba2291a663572c56c2a
Will <will.lucen4@gmail.com>
2021-04-21 11:55:25 -0300

    add styles

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <p>{{ description }}</p>
-    <img :src="image" alt="icon" />
+  <div
+    class="card"
+    :class="{
+      'card--cyan': cyan,
+      'card--red': red,
+      'card--orange': orange,
+      'card--blue': blue,
+    }"
+  >
+    <h1 class="title">{{ title }}</h1>
+    <p class="description">{{ description }}</p>
+    <img :src="image" alt="icon" class="image" width="50px" height="50px" />
   </div>
 </template>
 
@@ -21,6 +29,24 @@ export default {
       type: String,
       default: undefined,
     },
+    color: {
+      type: String,
+      default: undefined,
+    },
+  },
+  data() {
+    return {
+      cyan: false,
+      blue: false,
+      orange: false,
+      red: false,
+      colors: {
+        cyan: () => (this.cyan = true),
+        blue: () => (this.blue = true),
+        orange: () => (this.orange = true),
+        red: () => (this.red = true),
+      },
+    };
   },
   computed: {
     image() {
@@ -30,7 +56,61 @@ export default {
       return require("@/assets/images/image-default.png");
     },
   },
+  mounted() {
+    this.colors[this.color]();
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/assets/scss/styles.scss";
+@import "@/assets/scss/base.scss";
+
+.card {
+  background-color: $grey-ligher;
+  margin: 1rem;
+  min-height: 200px;
+  border-radius: 8px;
+  border-top: 4px solid;
+  border-top-color: $grey-ligher;
+}
+
+.card--cyan {
+  border-top-color: $cyan;
+}
+
+.card--red {
+  border-top-color: $red;
+}
+
+.card--orange {
+  border-top-color: $orange;
+}
+
+.card--blue {
+  border-top-color: $blue;
+}
+
+.title {
+  font-family: $font-family;
+  color: $blue-darker;
+  font-weight: 600;
+  font-size: 1.5rem;
+  flex: 1;
+}
+
+.description {
+  font-family: $font-family;
+  color: $grey;
+  font-weight: 400;
+  font-size: 1rem;
+
+  flex: 1;
+}
+
+.image {
+  flex: 10;
+  align-self: flex-end;
+  object-fit: contain;
+}
+</style>
