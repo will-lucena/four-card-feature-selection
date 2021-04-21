ceb92bc50d858656498077e4905fb7b39d8107b8
Will <will.lucen4@gmail.com>
2021-04-21 11:17:24 -0300

    add styles

--- src/components/Sentence.vue
+++ src/components/Sentence.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <p v-for="(piece, index) in pieces" :key="index">{{ piece.content }}</p>
+  <div class="header-box">
+    <p
+      v-for="(piece, index) in pieces"
+      :key="index"
+      class="text"
+      :class="{ 'text--bold': piece.isBold, 'text--small': piece.isSmall }"
+    >
+      {{ piece.content }}
+    </p>
   </div>
 </template>
 
@@ -15,4 +22,30 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import "@/assets/scss/styles.scss";
+@import "@/assets/scss/base.scss";
+
+.header-box {
+  margin: 2rem 0;
+}
+
+.text {
+  font-family: $font-family;
+  font-weight: 200;
+  font-size: 1.5rem;
+  color: $grey;
+  text-align: center;
+  margin: 0.5rem 0;
+}
+
+.text--bold {
+  font-weight: 600;
+  color: $blue-darker;
+}
+
+.text--small {
+  font-size: 1rem;
+  font-weight: 400;
+}
+</style>
